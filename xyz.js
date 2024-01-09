50a064b2beadaa5010a9189afe9809432bb29856
Vaishnavi Patil <93943926+vpvaish@users.noreply.github.com>
2024-01-09 13:23:13 +0530

    Add files via upload

---
+++ xyz.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add code to handle form submission here
+    // You can use AJAX or any backend service for form submission
+    alert('Form submitted!');
+});
